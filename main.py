bec58ee31669dd6b0d868d3483b974f56a27b522
stancadorin47 <73275960+stancadorin47@users.noreply.github.com>
2021-11-24 10:52:29 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,52 +1,48 @@
-from ProgramInternalForm import PIF
-from Scanner import *
-from SymbolTable import SymbolTable
+def closure(I, nonTerminals):
+
+    for item in I:
+        index = item[1].index('.')
+        if item[1][index + 1] in nonTerminals:
+            for production in nonTerminals[item[1][index + 1]]:
+                if [item[1][index + 1], str('.') + str(production)] not in I:
+                    I.append([item[1][index + 1], str('.') + str(production)])
+
+    return I
+
+state = []
+I = []
+
+def setOfItems(start, nonTerminals, terminals):
+    I.append(closure([['start', '.' + start]], nonTerminals))
+
+    terminals += list(nonTerminals.keys())
+
+    for conI in I:
+        for grammar in terminals:
+            if(grammar is '$'):
+                continue
+            goto = False
+
+            close = []
+            for item in conI:
+                if(item[1].index('.')<(len(item[1])-1)):
+                    close.append([item[0],item[1][:item[1].index('.')]])
+            l = closure(close,nonTerminals)
+            if(len(l) == 0):
+                continue
+            if(grammar in nonTerminals.keys()):
+                goto = True
+            if(l not in I):
+                if(goto):
+                    state.append(['g',I.index(conI)+1,len(I)+1,grammar])
+                    goto = True
+
+                I.append(l)
+
+            else:
+               if(goto):
+                    goto = True
+                    state.append(['g',I.index(conI)+1,I.index(l)+1,grammar])
 
 
 
-class Main:
-
-    def __init__(self):
-        self.st = SymbolTable(17)
-        self.pif = PIF()
-        self.scanner = Scanner()
-
-    def run(self):
-        readFile()
-        fileName = "p1.txt"
-        exceptionMessage = ""
-
-
-        with open(fileName, 'r') as file:
-            lineCounter = 0
-            for line in file:
-                lineCounter += 1
-                tokens = self.scanner.tokenize(line.strip())
-                for i in range(len(tokens)):
-                    if tokens[i] in reservedWords+separators+operators:
-                        if tokens[i] == ' ':
-                            continue
-                        self.pif.add(tokens[i], (-1, -1))
-                    elif tokens[i] in self.scanner.cases and i<len(tokens)-1:
-                        if re.match("[1-9]", tokens[i+1]):
-                            self.pif.add(tokens[i][:-1], (-1, -1))
-                            continue
-                        else:
-                            exceptionMessage += 'Lexical error at token ' + tokens[i] + ', at line ' + str(lineCounter) + "\n"
-                    else:
-                        exceptionMessage += 'Lexical error at token ' + tokens[i] + ', at line ' + str(lineCounter) + "\n"
-
-        with open('st.out', 'w') as writer:
-            writer.write(str(self.st))
-
-        with open('pif.out', 'w') as writer:
-            writer.write(str(self.pif))
-
-        if exceptionMessage == '':
-            print("Lexically correct")
-        else:
-            print(exceptionMessage)
-
-
-main = Main()
-main.run()
